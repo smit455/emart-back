a72defb5bb015e6b7716e62f0ad60a686081bd0e
smit ajani <121047896+smit455@users.noreply.github.com>
2024-06-24 14:31:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(cors({
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
     preflightContinue: false,
     optionsSuccessStatus: 204
   }));
